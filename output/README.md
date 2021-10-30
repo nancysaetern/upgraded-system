e59cc1e949de4edb874d370b689b0a696d3453f1
Nancy <nancysaetern@gmail.com>
2021-10-30 14:00:55 -0700

    badges and licenses added

--- output/README.md
+++ output/README.md
@@ -1,2 +1,28 @@
-# test
+# readme 
+ 
+## Description 
+node based application 
+ 
+## Table of Contents 
+* [Installation](#installation) 
+* [Usage](#usage) 
+* [Contributing](#contributing) 
+* [Tests](#tests) 
+* [Questions](#questions) 
+ 
+## Installation 
+you will need node 
+ 
+## Usage 
+9 questions asked 
+ 
+## Contributing 
+clone for personal use 
+ 
+## Tests 
+no tesets 
+ 
+## Questions 
+testing [GitHub Profile](https://github.com/testing) 
 
+Feel free to email me at testing 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,119 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case "Apache 2.0":
+      return "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+    case "GNU":
+      return "[![License: GNU](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+    default:
+      return "";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  switch(license) {
+    case "Apache 2.0":
+      return "(https://opensource.org/licenses/Apache-2.0)";
+    case "GNU":
+      return "(https://www.gnu.org/licenses/gpl-3.0)";
+    case "MIT":
+      return "(https://opensource.org/licenses/MIT)";
+    default:
+      return "";
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+
+function renderLinkedLicenseBadge(license) {
+  switch(license) {
+    case "No License":
+      return "";
+    default:
+      return `${renderLicenseBadge(license)}${renderLicenseLink(license)} \n\n`;
+  }
+}
+
+
+function renderLicenseSection(license) {
+  switch(license) {
+    case "No License":
+      return "";
+    default:
+      return `## License \n`+
+        `Click the badge for more information about the license. \n\n`+
+        `${renderLinkedLicenseBadge(license)}`;
+  }
+}
+
+
+
+function renderTitle(title) { 
+  return `# ${title} \n`;
+}
+
+function renderDescription(description) { 
+  return `## Description \n`+
+    `${description} \n`;
+}
+
+function renderTableOfContents(license) { 
+  
+  switch (license) {
+    case "No License":
+      licenseTemplate = "";
+      break;
+    default:
+      licenseTemplate = `* [License](#license) \n`;
+  }
+
+  return `## Table of Contents \n`+
+    `* [Installation](#installation) \n`+
+    `* [Usage](#usage) \n`+
+    licenseTemplate +
+    `* [Contributing](#contributing) \n`+
+    `* [Tests](#tests) \n`+
+    `* [Questions](#questions) \n`;
+}
+
+function renderInstallation(install) { 
+  return `## Installation \n`+
+    `${install} \n`;
+}
+
+function renderUsage(usage) { 
+  return `## Usage \n`+
+    `${usage} \n`;
+}
+
+function renderContributing(contribute) { 
+  return `## Contributing \n`+
+    `${contribute} \n`;
+}
+
+function renderTests(tests) { 
+  return `## Tests \n`+
+    `${tests} \n`;
+}
+
+function renderQuestions(username, email) { 
+  return `## Questions \n`+
+    `${username} [GitHub Profile](https://github.com/${username}) \n\n`+
+    `Feel free to email me at ${email} \n`;
+}
+
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `${renderTitle(data.title)} \n`+
+    `${renderLinkedLicenseBadge(data.license)}`+
+    `${renderDescription(data.description)} \n`+
+    `${renderTableOfContents(data.license)} \n`+
+    `${renderInstallation(data.installation)} \n`+
+    `${renderUsage(data.usage)} \n`+
+    `${renderLicenseSection(data.license)}`+
+    `${renderContributing(data.contribute)} \n`+
+    `${renderTests(data.tests)} \n`+
+    `${renderQuestions(data.username, data.email)}`;
 }
 
 module.exports = generateMarkdown;
